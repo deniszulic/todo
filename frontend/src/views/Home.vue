78d70a535f999344a7e5f4e6eb1607c68468454e
deniszulic <35138382+deniszulic@users.noreply.github.com>
2022-03-30 19:07:44 +0200

    design

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Todo />
+  </div>
+</template>
+
+<script>
+import Todo from "@/views/Todo.vue";
+
+export default {
+  name: 'Home',
+  components: {
+    Todo
+  }
+}
+</script>
---
+++ frontend/src/views/Todo.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <div class="center col-6">
+      <div class="shadow p-3 mb-5 bg-white rounded">
+        <div class="p-3 mb-2 bg-primary text-white size">Todo</div>
+        <div class="form-row">
+          <div class="form-group col-md-10">
+            <input type="text" class="form-control" placeholder="Add a task" />
+          </div>
+          <div class="form-group col-md-2">
+            <button type="submit" class="btn btn-primary form-control">
+              <i class="fa-solid fa-check"></i>
+            </button>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <div class="input-group-text">
+              <input
+                type="checkbox"
+                aria-label="Checkbox for following text input"
+              />
+            </div>
+          </div>
+          <input
+            type="text"
+            class="form-control"
+            aria-label="Text input with checkbox"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+.center {
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+}
+.size {
+  font-size: 30px;
+}
+</style>
