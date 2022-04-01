e3f5469527c3aaf6ceb744e533462884e105a133
deniszulic <35138382+deniszulic@users.noreply.github.com>
2022-04-01 14:08:34 +0200

    port

--- backend/util/database.js
+++ backend/util/database.js
@@ -3,6 +3,7 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize("todo", "postgres", "admin", {
     dialect: "postgres",
     host: "localhost",
+    port: '5431'
 });
 
 module.exports = sequelize;
